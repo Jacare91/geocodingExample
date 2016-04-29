58ad22c0acd6197416cc0cabfa3159e3d9c77664
Tomek Spędzia <tomasz.spedzia@comarch.com>
2016-04-29 21:21:39 +0200

    First geocoding part is done! :)

--- app/src/main/java/jacare/geocodingexample/LocationHelper.java
+++ app/src/main/java/jacare/geocodingexample/LocationHelper.java
@@ -23,140 +23,42 @@ import java.util.Locale;
 public class LocationHelper {
     public static final String TAG = "LocHelp";
 
-    protected Context context;
-    protected LinkedHashMap<LatLng, String> addresssList;
-    protected Address currLoc;
+    private Context context;
 
-    public LocationHelper(Context context){
+    public LocationHelper(Context context) {
         this.context = context;
-        addresssList = new LinkedHashMap<>();
     }
 
-    public Location convertAddressToLocation(Address address){
-        Location location = new Location("");
-        location.setLatitude(address.getLatitude());
-        location.setLongitude(address.getLongitude());
+    public void convertLatLngToAddress(final LatLng location, final AddressListener listener) {
+        Log.i(TAG, "Converting input to address!");
 
-        return location;
-    }
-
-    public void convertAddressStringToLatLng(final String addressString, final Location stuffLocation,
-                                             final LocationListener listener) {
-        new AsyncTask<Void, Void, LatLng>() {
+        new AsyncTask<Void, Void, Address>() {
             @Override
-            protected LatLng doInBackground(Void... params) {
-                if(addressString.equals(""))
-                    return null;
+            protected Address doInBackground(Void... params) {
+                Geocoder geocoder = new Geocoder(context, Locale.getDefault());
                 try {
-                    Geocoder gc = new Geocoder(context);
-
-                    if (Geocoder.isPresent()) {
-                        Log.d(TAG, "Obtaining address.");
-
-                        List<Address> list = null;
-                        String builtAddress = buildAddressString(addressString);
-
-                        if(builtAddress != null)
-                            list = gc.getFromLocationName(builtAddress, 20);
-
-                        if (list == null || list.size() == 0) {
-                            Log.d(TAG, "No addresses!");
-                            listener.onAddressNotFound();
-                            return null;
-                        }
-                        else {
-                            Log.d(TAG, "Parsing addresses.");
-
-                            Address address = list.get(0);
-                            Location location = convertAddressToLocation(list.get(0));
-                            double distanceFrom = stuffLocation.distanceTo(location);
-                            for (int i = 1; i < list.size(); i++) {
-                                location = convertAddressToLocation(list.get(i));
-                                double tmpDist = stuffLocation.distanceTo(location);
-                                if (tmpDist < distanceFrom) {
-                                    distanceFrom = tmpDist;
-                                    address = list.get(i);
-                                }
-                            }
-
-                            Log.d(TAG, "Hit address! Obtained address is " + location.toString());
-                            return new LatLng(address.getLatitude(), address.getLongitude());
-                        }
+                    List<Address> addresses = geocoder.getFromLocation(location.latitude, location.longitude, 1);
+                    if (addresses.size() > 0) {
+                        Address address = addresses.get(0);
+                        return address;
                     }
                 } catch (IOException e) {
+                    e.printStackTrace();
                     Log.d(TAG, "Something fucked up with geocoder!");
                 }
-
                 return null;
             }
 
             @Override
-            protected void onPostExecute(LatLng latLng) {
-                if(latLng != null)
-                    listener.onConversionFinished(latLng);
-                else
-                    listener.onAddressNotFound();
+            protected void onPostExecute(Address address) {
+                if (listener != null && address != null)
+                    listener.onConversionFinished(address);
             }
         }.execute();
+
     }
 
-    public void convertLatLngToAddress(final LatLng location, final AddressListener listener) {
-        if (!addresssList.containsKey(location)) {
-            new AsyncTask<Void, Void, Address>() {
-                @Override
-                protected Address doInBackground(Void... params) {
-                    Geocoder geocoder = new Geocoder(context, Locale.getDefault());
-                    try {
-                        List<Address> addresses = geocoder.getFromLocation(location.latitude, location.longitude, 1);
-                        if (addresses.size() > 0) {
-                            Address address = addresses.get(0);
-                            currLoc = address;
-                            return address;
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        Log.d(TAG, "Something fucked up with geocoder!");
-                    }
-                    return null;
-                }
-
-                @Override
-                protected void onPostExecute(Address address) {
-                    if(listener != null && address != null)
-                        listener.onConversionFinished(address);
-                }
-            }.execute();
-        }
-    }
-
-    protected String buildAddressString(String address){
-        if(currLoc != null) {
-            if (address.contains(", " + currLoc.getLocality()))
-                return address;
-            else if(address.contains(", "))
-                return address;
-            else {
-                return address + ", " + currLoc.getLocality();
-            }
-        }
-        else
-            return null;
-    }
-
-    public static Location buildLocation(LatLng latLng, String provider){
-        Location location = new Location(provider);
-        location.setLatitude(latLng.latitude);
-        location.setLongitude(latLng.longitude);
-
-        return location;
-    }
-
-    public interface LocationListener{
-        void onConversionFinished(LatLng latLng);
-        void onAddressNotFound();
-    }
-
-    public interface AddressListener{
+    public interface AddressListener {
         void onConversionFinished(Address address);
     }
 }
--- app/src/main/java/jacare/geocodingexample/MapsActivity.java
+++ app/src/main/java/jacare/geocodingexample/MapsActivity.java
@@ -1,5 +1,6 @@
 package jacare.geocodingexample;
 
+import android.location.Address;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -11,19 +12,24 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapClickListener {
-    @BindView(R.id.address_output)EditText addressOutput;
+    private EditText destAddressContainer;
 
+    private LocationHelper locationHelper;
     private GoogleMap map;
-    private Marker marker;
-    private String address;
+
+    private Marker destMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        destAddressContainer = (EditText)findViewById(R.id.address);
+
         setUpMapIfNeeded();
+        locationHelper = new LocationHelper(this);
     }
 
     @Override
@@ -33,16 +39,25 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapCli
     }
 
     private void setUpMapIfNeeded() {
-        if (map == null) {
-            map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-        }
+        if (map == null)
+            map = ((SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map)).getMap();
+        map.setOnMapClickListener(this);
     }
 
     @Override
     public void onMapClick(LatLng latLng) {
-        if(marker != null)
-            marker.remove();
-        marker = map.addMarker(new MarkerOptions().position(latLng));
+        if(destMarker != null)
+            destMarker.remove();
+        destMarker = map.addMarker(new MarkerOptions().position(latLng));
+        locationHelper.convertLatLngToAddress(latLng, new LocationHelper.AddressListener() {
+            @Override
+            public void onConversionFinished(Address address) {
+                if(address != null) {
+                    String addressLine = address.getAddressLine(0);
+                    destAddressContainer.setText(addressLine);
+                }
+            }
+        });
     }
 }
